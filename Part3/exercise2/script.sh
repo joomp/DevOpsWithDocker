72c41dec66fce06575537250fce11f82cb532659
joomp <65067816+joomp@users.noreply.github.com>
2022-03-26 17:02:52 +0200

    Update script.sh

--- Part3/exercise2/script.sh
+++ Part3/exercise2/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git clone $REPOSITORY_URL $IMAGE_NAME
-cd $IMAGE_NAME
-rm -rf .git
+git clone $REPOSITORY_URL $IMAGE_NAME;
+cd $IMAGE_NAME;
+rm -rf .git;
 
 docker build . -t "$DOCKER_HUB_USERNAME/$IMAGE_NAME";
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD;
